f60b2c2cdd397c5026a6465a11fdaf85bedb17ae
predy-dev <predy-protocol@protonmail.com>
2022-02-16 12:13:06 +0900

    Updated tasks

--- tasks/pool.ts
+++ tasks/pool.ts
@@ -19,6 +19,15 @@ task("pool", "get pool status")
     const perpetualMarket = await getPerpetualMarket(ethers, deployer, network.name)
 
     const lpTokenPrice = await perpetualMarket.getLPTokenPrice(0)
+    const result = await perpetualMarket.getTokenAmountForHedging()
 
     console.log('LPToken Price : $', toUnscaled(lpTokenPrice.div('100000000'), 8).toLocaleString())
+    console.log('Hedge         :')
+    if (result[0]) {
+      console.log(` Receive     : ${toUnscaled(result[1], 6).toLocaleString()} USDC`)
+      console.log(` Required    : ${toUnscaled(result[2].div('10000000000'), 8).toLocaleString()} WETH`)
+    } else {
+      console.log(` Required    : ${toUnscaled(result[1], 6).toLocaleString()} USDC`)
+      console.log(` Receive     : ${toUnscaled(result[2].div('10000000000'), 8).toLocaleString()} WETH`)
+    }
   })
--- tasks/vault.ts
+++ tasks/vault.ts
@@ -24,24 +24,24 @@ task("vault", "get vault status")
     console.log('Value          : $', toUnscaled(vaultStatus.positionValue, 8).toLocaleString())
     console.log('MinCollateral  : $', toUnscaled(vaultStatus.minCollateral, 8).toLocaleString())
 
+    console.log('PositionValues')
     for (let i = 0; i < vaultStatus.positionValues.length; i++) {
-      console.log('PositionValues')
       console.log(` SubVault: ${i}`)
       console.log('  Sqeeth : $', toUnscaled(vaultStatus.positionValues[i][0], 8).toLocaleString())
       console.log('  Future : $', toUnscaled(vaultStatus.positionValues[i][1], 8).toLocaleString())
     }
 
+    console.log('FundingPaid')
     for (let i = 0; i < vaultStatus.fundingPaid.length; i++) {
-      console.log('FundingPaid')
       console.log(` SubVault: ${i}`)
       console.log('  Sqeeth : $', toUnscaled(vaultStatus.fundingPaid[i][0], 8).toLocaleString())
       console.log('  Future : $', toUnscaled(vaultStatus.fundingPaid[i][1], 8).toLocaleString())
     }
 
+    console.log('Positions')
     for (let i = 0; i < vaultStatus.rawVaultData.subVaults.length; i++) {
-      console.log('Positions')
       console.log(` SubVault: ${i}`)
-      console.log('  Sqeeth :  ', toUnscaled(vaultStatus.rawVaultData.subVaults[0].positionPerpetuals[0], 8).toLocaleString())
-      console.log('  Future :  ', toUnscaled(vaultStatus.rawVaultData.subVaults[0].positionPerpetuals[1], 8).toLocaleString())
+      console.log('  Sqeeth :  ', toUnscaled(vaultStatus.rawVaultData.subVaults[i].positionPerpetuals[0], 8).toLocaleString())
+      console.log('  Future :  ', toUnscaled(vaultStatus.rawVaultData.subVaults[i].positionPerpetuals[1], 8).toLocaleString())
     }
   })
