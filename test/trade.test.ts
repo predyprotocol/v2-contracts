71d3e799756d2cabfccccd145aac16045385e050
predy-dev <predy-protocol@protonmail.com>
2022-03-06 12:33:49 +0900

    test: check the vault gets correct payoff.

--- test/trade.test.ts
+++ test/trade.test.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { MockERC20, PerpetualMarket } from '../typechain'
-import { Wallet } from 'ethers'
+import { BigNumber, Wallet } from 'ethers'
 import {
   deployTestContractSet,
   restoreSnapshot,
@@ -9,8 +9,15 @@ import {
   TestContractHelper,
   TestContractSet,
 } from './utils/deploy'
-import { scaledBN } from './utils/helpers'
-import { FUTURE_PRODUCT_ID, MAX_WITHDRAW_AMOUNT, MIN_MARGIN, SQUEETH_PRODUCT_ID } from './utils/constants'
+import { assertCloseToPercentage, increaseTime, numToBn, scaledBN } from './utils/helpers'
+import {
+  FUNDING_PERIOD,
+  FUTURE_PRODUCT_ID,
+  MAX_WITHDRAW_AMOUNT,
+  MIN_MARGIN,
+  SAFETY_PERIOD,
+  SQUEETH_PRODUCT_ID,
+} from './utils/constants'
 
 describe('trade', function () {
   let wallet: Wallet
@@ -55,39 +62,188 @@ describe('trade', function () {
   })
 
   describe('trade', () => {
+    const depositAmount = scaledBN(50000000, 6)
+
     beforeEach(async () => {
-      await perpetualMarket.initialize(scaledBN(50000000, 6), scaledBN(2, 5))
+      await perpetualMarket.initialize(depositAmount, 100000)
     })
 
-    describe('tokenPrice becomes high', () => {
-      const subVaultIndex = 0
+    describe('payoff check', () => {
+      const initialSpotPrice = 1000
 
-      beforeEach(async () => {
-        await testContractHelper.updateSpot(scaledBN(1000, 8))
+      async function checkPayoff(tradeAmounts: number[], spotPrice: number, vaultId: number, subVaultIndex: number) {
+        await testContractHelper.updateSpot(scaledBN(initialSpotPrice, 8))
 
+        const beforeLPTokenPrice = await perpetualMarket.getLPTokenPrice(depositAmount.mul(-1))
+        await openPosition(tradeAmounts, vaultId, subVaultIndex)
+
+        await increaseTime(SAFETY_PERIOD)
+
+        // Close position and check payoff
+        const before = await usdc.balanceOf(wallet.address)
+        await closePosition(spotPrice, vaultId, subVaultIndex)
+        const after = await usdc.balanceOf(wallet.address)
+        const afterLPTokenPrice = await perpetualMarket.getLPTokenPrice(depositAmount.mul(-1))
+
+        const expectedPayoff = getExpectedPayout(tradeAmounts, spotPrice)
+        const vaultProfit = after.sub(before).sub(MIN_MARGIN)
+        const lpProfit = afterLPTokenPrice.sub(beforeLPTokenPrice).mul(depositAmount).div(scaledBN(1, 16))
+
+        // Assert trader's profit and loss
+        assertCloseToPercentage(vaultProfit, numToBn(expectedPayoff.profit, 6), BigNumber.from(500000))
+
+        // Assert LP's profit and loss
+        assertCloseToPercentage(
+          lpProfit,
+          numToBn(-expectedPayoff.profit - expectedPayoff.protocolFee, 6),
+          BigNumber.from(500000),
+        )
+
+        // Check the vault has no positions
+        const traderVault = await perpetualMarket.getTraderVault(vaultId)
+        expect(traderVault.positionUsdc).to.be.eq(0)
+        expect(traderVault.subVaults[0].positionPerpetuals[0]).to.be.eq(0)
+        expect(traderVault.subVaults[0].positionPerpetuals[1]).to.be.eq(0)
+      }
+
+      async function checkFundingPayment(
+        tradeAmounts: number[],
+        spotPrice: number,
+        isPoolReceived: boolean,
+        isTraderReceived: boolean,
+        vaultId: number,
+        subVaultIndex: number,
+      ) {
+        await testContractHelper.updateSpot(scaledBN(initialSpotPrice, 8))
+
+        const beforeLPTokenPrice = await perpetualMarket.getLPTokenPrice(depositAmount.mul(-1))
+        await openPosition(tradeAmounts, vaultId, subVaultIndex)
+
+        await increaseTime(FUNDING_PERIOD * 100)
+
+        // Close position and check payoff
+        const before = await usdc.balanceOf(wallet.address)
+        await closePosition(spotPrice, vaultId, subVaultIndex)
+        const after = await usdc.balanceOf(wallet.address)
+        const afterLPTokenPrice = await perpetualMarket.getLPTokenPrice(depositAmount.mul(-1))
+
+        const expectedPayoff = getExpectedPayout(tradeAmounts, spotPrice)
+
+        const vaultFundingReceived = after.sub(before).sub(MIN_MARGIN).sub(numToBn(expectedPayoff.profit, 6))
+        const lpFundingFundingReceived = afterLPTokenPrice
+          .sub(beforeLPTokenPrice)
+          .mul(depositAmount)
+          .div(scaledBN(1, 16))
+          .add(numToBn(expectedPayoff.profit, 6))
+          .add(numToBn(expectedPayoff.protocolFee, 6))
+
+        if (isPoolReceived) {
+          expect(lpFundingFundingReceived).to.be.gt(0)
+        } else {
+          expect(lpFundingFundingReceived).to.be.lt(0)
+        }
+
+        if (isTraderReceived) {
+          expect(vaultFundingReceived).to.be.gt(0)
+        } else {
+          expect(vaultFundingReceived).to.be.lt(0)
+        }
+
+        // Check the vault has no positions
+        const traderVault = await perpetualMarket.getTraderVault(vaultId)
+        expect(traderVault.positionUsdc).to.be.eq(0)
+        expect(traderVault.subVaults[0].positionPerpetuals[0]).to.be.eq(0)
+        expect(traderVault.subVaults[0].positionPerpetuals[1]).to.be.eq(0)
+      }
+
+      function getExpectedPayout(tradeAmounts: number[], spotPrice: number) {
+        const expectedPayoffOfFuture = tradeAmounts[0] * (spotPrice - initialSpotPrice)
+        const expectedPayoffOfSquared = (tradeAmounts[1] * (spotPrice ** 2 - initialSpotPrice ** 2)) / 10000
+        const tradeFeeRate = 0.0005
+        const protocolFeeRate = 0.4
+        const tradeFeeFuture = 2 * tradeFeeRate * Math.abs(tradeAmounts[0]) * initialSpotPrice
+        const tradeFeeSquared = (4 * tradeFeeRate * Math.abs(tradeAmounts[1]) * initialSpotPrice ** 2) / 10000
+        const tradeFee = tradeFeeFuture + tradeFeeSquared
+        const expectedProfit = expectedPayoffOfFuture + expectedPayoffOfSquared - tradeFee
+        const expectedProtocolFee = protocolFeeRate * tradeFee
+
+        return {
+          profit: expectedProfit,
+          protocolFee: expectedProtocolFee,
+        }
+      }
+
+      async function openPosition(tradeSmounts: number[], vaultId: number, subVaultIndex: number) {
         await perpetualMarket.trade({
-          vaultId: 0,
+          vaultId,
           trades: [
             {
               productId: FUTURE_PRODUCT_ID,
               subVaultIndex,
-              tradeAmount: scaledBN(2, 8),
+              tradeAmount: numToBn(tradeSmounts[0], 8),
+              limitPrice: 0,
+            },
+            {
+              productId: SQUEETH_PRODUCT_ID,
+              subVaultIndex,
+              tradeAmount: numToBn(tradeSmounts[1], 8),
               limitPrice: 0,
             },
           ],
           marginAmount: MIN_MARGIN,
           deadline: 0,
         })
+      }
 
-        await testContractHelper.updateSpot(scaledBN(950, 8))
+      async function closePosition(spotPrice: number, vaultId: number, subVaultIndex: number) {
+        const traderVault = await perpetualMarket.getTraderVault(vaultId)
 
+        await testContractHelper.updateSpot(numToBn(spotPrice, 8))
+
+        await perpetualMarket.trade({
+          vaultId,
+          trades: [
+            {
+              productId: FUTURE_PRODUCT_ID,
+              subVaultIndex,
+              tradeAmount: traderVault.subVaults[subVaultIndex].positionPerpetuals[0].mul(-1),
+              limitPrice: 0,
+            },
+            {
+              productId: SQUEETH_PRODUCT_ID,
+              subVaultIndex,
+              tradeAmount: traderVault.subVaults[subVaultIndex].positionPerpetuals[1].mul(-1),
+              limitPrice: 0,
+            },
+          ],
+          marginAmount: MAX_WITHDRAW_AMOUNT,
+          deadline: 0,
+        })
+        await increaseTime(SAFETY_PERIOD)
+      }
+
+      beforeEach(async () => {
+        await testContractHelper.updateSpot(scaledBN(initialSpotPrice, 8))
+        await perpetualMarket.trade({
+          vaultId: 0,
+          trades: [
+            {
+              productId: FUTURE_PRODUCT_ID,
+              subVaultIndex: 0,
+              tradeAmount: numToBn(1, 8),
+              limitPrice: 0,
+            },
+          ],
+          marginAmount: MIN_MARGIN,
+          deadline: 0,
+        })
         await perpetualMarket.trade({
           vaultId: 1,
           trades: [
             {
               productId: FUTURE_PRODUCT_ID,
-              subVaultIndex,
-              tradeAmount: scaledBN(-2, 8),
+              subVaultIndex: 0,
+              tradeAmount: numToBn(-1, 8),
               limitPrice: 0,
             },
           ],
@@ -96,16 +252,132 @@ describe('trade', function () {
         })
       })
 
-      it('withdraw all and check balance of PerpetualMarket', async () => {
-        const tokenAmount = await testContractSet.perpetualMarketCore.balanceOf(wallet.address)
-        const withdrawnAmount = await testContractHelper.getWithdrawalAmount(tokenAmount, 0)
+      it('trader and LP gets correct payoff', async () => {
+        const testDataSet = [
+          { tradeAmounts: [1, 0], spotPrice: 1000 },
+          { tradeAmounts: [0, 1], spotPrice: 1000 },
+          { tradeAmounts: [1, 0], spotPrice: 1100 },
+          { tradeAmounts: [0, 1], spotPrice: 1100 },
+          { tradeAmounts: [1, 0], spotPrice: 900 },
+          { tradeAmounts: [0, 1], spotPrice: 900 },
+          { tradeAmounts: [-1, 0], spotPrice: 900 },
+          { tradeAmounts: [-1, 0], spotPrice: 1100 },
+          { tradeAmounts: [0, -2], spotPrice: 900 },
+          { tradeAmounts: [0, -2], spotPrice: 1100 },
+          { tradeAmounts: [1, -2], spotPrice: 1100 },
+          { tradeAmounts: [1, -2], spotPrice: 900 },
+          { tradeAmounts: [-1, 2], spotPrice: 1100 },
+          { tradeAmounts: [-1, 2], spotPrice: 900 },
+        ]
 
-        await perpetualMarket.withdraw(withdrawnAmount)
+        for (let testData of testDataSet) {
+          await checkPayoff(testData.tradeAmounts, testData.spotPrice, 1, 0)
+        }
+      })
 
-        expect(withdrawnAmount).to.gt(scaledBN(200000, 6))
+      it('trader and LP gets correct payoff(multiple subVaults)', async () => {
+        const testDataSet = [
+          { tradeAmounts: [1, 0], spotPrice: 1000 },
+          { tradeAmounts: [0, 1], spotPrice: 1000 },
+          { tradeAmounts: [1, 0], spotPrice: 1100 },
+          { tradeAmounts: [0, 1], spotPrice: 1100 },
+          { tradeAmounts: [1, 0], spotPrice: 900 },
+          { tradeAmounts: [0, 1], spotPrice: 900 },
+          { tradeAmounts: [-1, 0], spotPrice: 900 },
+          { tradeAmounts: [-1, 0], spotPrice: 1100 },
+          { tradeAmounts: [0, -2], spotPrice: 900 },
+          { tradeAmounts: [0, -2], spotPrice: 1100 },
+          { tradeAmounts: [1, -2], spotPrice: 1100 },
+          { tradeAmounts: [1, -2], spotPrice: 900 },
+          { tradeAmounts: [-1, 2], spotPrice: 1100 },
+          { tradeAmounts: [-1, 2], spotPrice: 900 },
+        ]
 
-        expect(await usdc.balanceOf(perpetualMarket.address)).to.eq(0)
-        expect(await testContractSet.perpetualMarketCore.balanceOf(wallet.address)).to.eq(0)
+        let subVaultIndex = 0
+
+        for (let testData of testDataSet) {
+          await checkPayoff(testData.tradeAmounts, testData.spotPrice, 1, subVaultIndex++)
+        }
+      })
+
+      it('trader and LP gets correct funding received', async () => {
+        const testDataSet = [
+          { tradeAmounts: [2, 0], spotPrice: 1000, isPoolReceived: true, isTraderReceived: false },
+          { tradeAmounts: [0, 2], spotPrice: 1000, isPoolReceived: true, isTraderReceived: false },
+          { tradeAmounts: [-2, 0], spotPrice: 1000, isPoolReceived: true, isTraderReceived: false },
+          { tradeAmounts: [0, -2], spotPrice: 1000, isPoolReceived: false, isTraderReceived: true },
+        ]
+
+        for (let testData of testDataSet) {
+          await checkFundingPayment(
+            testData.tradeAmounts,
+            testData.spotPrice,
+            testData.isPoolReceived,
+            testData.isTraderReceived,
+            1,
+            0,
+          )
+        }
+      })
+
+      it("trader and LP gets correct funding received(future's funding rate is positive)", async () => {
+        await perpetualMarket.trade({
+          vaultId: 1,
+          trades: [
+            {
+              productId: FUTURE_PRODUCT_ID,
+              subVaultIndex: 0,
+              tradeAmount: numToBn(100, 8),
+              limitPrice: 0,
+            },
+          ],
+          marginAmount: scaledBN(20000, 6),
+          deadline: 0,
+        })
+        await perpetualMarket.trade({
+          vaultId: 0,
+          trades: [
+            {
+              productId: FUTURE_PRODUCT_ID,
+              subVaultIndex: 0,
+              tradeAmount: numToBn(1, 8),
+              limitPrice: 0,
+            },
+          ],
+          marginAmount: MIN_MARGIN,
+          deadline: 0,
+        })
+        await perpetualMarket.trade({
+          vaultId: 2,
+          trades: [
+            {
+              productId: FUTURE_PRODUCT_ID,
+              subVaultIndex: 0,
+              tradeAmount: numToBn(-1, 8),
+              limitPrice: 0,
+            },
+          ],
+          marginAmount: MAX_WITHDRAW_AMOUNT,
+          deadline: 0,
+        })
+
+        const testDataSet = [
+          { tradeAmounts: [2, 0], spotPrice: 1000, isPoolReceived: true, isTraderReceived: false },
+          { tradeAmounts: [0, 2], spotPrice: 1000, isPoolReceived: true, isTraderReceived: false },
+          { tradeAmounts: [-2, 0], spotPrice: 1000, isPoolReceived: true, isTraderReceived: true },
+          { tradeAmounts: [0, -2], spotPrice: 1000, isPoolReceived: false, isTraderReceived: true },
+        ]
+
+        for (let testData of testDataSet) {
+          await checkFundingPayment(
+            testData.tradeAmounts,
+            testData.spotPrice,
+            testData.isPoolReceived,
+            testData.isTraderReceived,
+            2,
+            0,
+          )
+        }
       })
     })
 
