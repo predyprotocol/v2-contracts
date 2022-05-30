1c3fc7148def8fb835811b0d40dadebfcdde2e4c
wshino <wshinohara@gmail.com>
2022-05-30 19:52:55 +0900

    add script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-forge install openzeppelin/openzeppelin-contracts@v3.4.2-solc-0.7
-forge install uniswap/v3-core@1.0.0
-forge install uniswap/v3-periphery
-forge install smartcontractkit/chainlink
+git submodule update
+cd lib/openzeppelin-contracts && git checkout refs/tags/v3.4.2-solc-0.7
