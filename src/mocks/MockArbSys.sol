fbd6e61a30d3cbbce3ce742e3fd6aa849f24fb2d
predy-dev <predy-protocol@protonmail.com>
2022-06-08 07:25:57 +0900

    style: apply lint command

--- src/mocks/MockArbSys.sol
+++ src/mocks/MockArbSys.sol
@@ -5,14 +5,13 @@ pragma solidity =0.7.6;
  * @notice Mock of Chainlink Aggregator contract
  */
 contract MockArbSys {
+    uint256 _blocknumber;
 
-    uint _blocknumber;
-
-    function arbBlockNumber() external view returns (uint) {
-      return _blocknumber;
+    function arbBlockNumber() external view returns (uint256) {
+        return _blocknumber;
     }
 
-    function setBlockNumber(uint blocknumber) external {
+    function setBlockNumber(uint256 blocknumber) external {
         _blocknumber = blocknumber;
     }
 }
